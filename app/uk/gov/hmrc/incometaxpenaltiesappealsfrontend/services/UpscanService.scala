a7fddf293a5488d709cc9dcd6f552494653c4257
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-01-22 17:47:38 +0000

    [MIPR-1473] Adding in UpscanService

---
+++ app/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/services/UpscanService.scala
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxpenaltiesappealsfrontend.services
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.models.upscan.{UploadJourney, UploadStatus, UploadStatusEnum}
+import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.repositories.FileUploadJourneyRepository
+import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.utils.ExceptionHandlingUtil
+import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.utils.Logger.logger
+import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.utils.PagerDutyHelper.PagerDutyKeys._
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class UpscanService @Inject()(uploadRepo: FileUploadJourneyRepository)
+                             (implicit ec: ExecutionContext) extends ExceptionHandlingUtil {
+
+  def upsertFileUpload(journeyId: String, uploadJourney: UploadJourney): Future[UploadJourney] =
+    withExceptionHandling(
+      methodName = "upsertFileUpload",
+      identifiers = Map("journeyId" -> journeyId, "fileReference" -> uploadJourney.reference),
+      pagerDutyTriggerKey = Some(FAILED_UPSERT_FILE_UPSCAN)
+    ) {
+      uploadRepo.upsertFileUpload(journeyId, uploadJourney).map { cacheItem =>
+        logger.debug(s"[UpscanService][upsertFileUpload] Upserted file for journeyId: $journeyId, JSON:\n\n" + Json.toJson(cacheItem.data))
+        uploadJourney
+      }
+    }
+
+  def getFile(journeyId: String, fileReference: String): Future[Option[UploadJourney]] =
+    withExceptionHandling("getFile", Map("journeyId" -> journeyId, "fileReference" -> fileReference)) {
+      uploadRepo.getFile(journeyId, fileReference).map {
+        case Some(file) =>
+          logger.debug(s"[UpscanService][getFile] Found a file for journeyId: $journeyId and fileReference: $fileReference, JSON:\n\n" + Json.toJson(file))
+          Some(file)
+        case None =>
+          logger.debug(s"[UpscanService][getFile] No file found for journeyId: $journeyId and fileReference: $fileReference")
+          None
+      }
+    }
+
+  def getAllFiles(journeyId: String): Future[Seq[UploadJourney]] =
+    withExceptionHandling("getAllFiles", Map("journeyId" -> journeyId)) {
+      uploadRepo.getAllFiles(journeyId).map { files =>
+        logger.debug(s"[UpscanService][getAllFiles] Found ${files.size} files for journeyId: $journeyId\n" + Json.toJson(files))
+        files
+      }
+    }
+
+  def getAllReadyFiles(journeyId: String): Future[Seq[UploadJourney]] =
+    getAllFiles(journeyId).map(_.filter(_.fileStatus == UploadStatusEnum.READY)).map { files =>
+      logger.debug(s"[UpscanService][getAllReadyFiles] Found ${files.size} with status ${UploadStatusEnum.READY} files for journeyId: $journeyId\n" + Json.toJson(files))
+      files
+    }
+
+  def countAllFiles(journeyId: String): Future[Int] =
+    getAllFiles(journeyId).map(_.size).map { count =>
+      logger.debug(s"[UpscanService][countAllFiles] Counted $count files for journeyId: $journeyId")
+      count
+    }
+
+  def countAllReadyFiles(journeyId: String): Future[Int] =
+    getAllReadyFiles(journeyId).map(_.size).map { count =>
+      logger.debug(s"[UpscanService][countAllReadyFiles] Counted $count READY files for journeyId: $journeyId")
+      count
+    }
+
+  def getStatusOfFileUpload(journeyId: String, fileReference: String): Future[Option[UploadStatus]] =
+    withExceptionHandling(
+      methodName = "getStatusOfFileUpload",
+      identifiers = Map("journeyId" -> journeyId, "fileReference" -> fileReference),
+      pagerDutyTriggerKey = Some(FILE_UPLOAD_STATUS_NOT_FOUND_UPSCAN)
+    ) {
+      getFile(journeyId, fileReference).map(_.map { upload =>
+        upload.failureDetails.fold {
+          logger.debug(s"[UpscanService][getStatusOfFileUpload] File upload status '${upload.fileStatus}' for journeyId: $journeyId and fileReference: $fileReference")
+          UploadStatus(upload.fileStatus.toString)
+        } { failure =>
+          logger.debug(s"[UpscanService][getStatusOfFileUpload] File upload failed reason '${failure.failureReason}' for journeyId: $journeyId and fileReference: $fileReference with message: " + failure.message)
+          UploadStatus(failure.failureReason.toString, Some(failure.message))
+        }
+      })
+    }
+
+  def getFormFieldsForFile(journeyId: String, fileReference: String): Future[Option[Map[String, String]]] =
+    getFile(journeyId, fileReference).map(_.flatMap(_.uploadFields)).map { formFields =>
+      logger.debug(s"[UpscanService][getFormFieldsForFile] Form fields for journeyId: $journeyId and fileReference: $fileReference\n\n" + formFields.map("\n" + _))
+      formFields
+    }
+
+  def removeFile(journeyId: String, fileReference: String): Future[Boolean] =
+    withExceptionHandling(
+      methodName = "removeFile",
+      identifiers = Map("journeyId" -> journeyId, "fileReference" -> fileReference),
+      pagerDutyTriggerKey = Some(FILE_REMOVAL_FAILURE_UPSCAN)
+    ) {
+      uploadRepo.removeFile(journeyId, fileReference).map { _ =>
+        logger.debug(s"[UpscanService][removeFile] Removed file for journeyId: $journeyId and fileReference: $fileReference")
+        true
+      }
+    }.recover(_ => false)
+
+  def removeAllFiles(journeyId: String): Future[Boolean] =
+    withExceptionHandling(
+      methodName = "removeAllFiles",
+      identifiers = Map("journeyId" -> journeyId),
+      pagerDutyTriggerKey = Some(FILE_REMOVAL_FAILURE_UPSCAN)
+    ) {
+      uploadRepo.removeAllFiles(journeyId).map { _ =>
+        logger.debug(s"[UpscanService][removeAllFiles] Removed file for journeyId: $journeyId")
+        true
+      }
+    }.recover(_ => false)
+
+  def getUploadedFilename(journeyId: String, fileReference: String): Future[Option[String]] =
+    withExceptionHandling(
+      methodName = "getUploadedFilename",
+      identifiers = Map("journeyId" -> journeyId, "fileReference" -> fileReference),
+      pagerDutyTriggerKey = Some(FILE_NAME_RETRIEVAL_FAILURE_UPSCAN)
+    ) {
+      getFile(journeyId, fileReference).map(_.flatMap(_.uploadDetails.map(_.fileName))).map { fileName =>
+        if (fileName.isDefined) logger.debug(s"[UpscanService][getFileNameForJourney] File name: ${fileName.get}, for journeyId: $journeyId and fileReference: $fileReference")
+        fileName
+      }
+    }
+}
--- it/test/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/repositories/FileUploadJourneyRepositoryISpec.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/repositories/FileUploadJourneyRepositoryISpec.scala
@@ -18,7 +18,6 @@ package uk.gov.hmrc.incometaxpenaltiesappealsfrontend.repositories
 
 import fixtures.FileUploadFixtures
 import play.api.test.Helpers._
-import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.models.upscan.{FailureReasonEnum, UploadStatus, UploadStatusEnum}
 import uk.gov.hmrc.incometaxpenaltiesappealsfrontend.utils.ComponentSpecHelper
 
 class FileUploadJourneyRepositoryISpec extends ComponentSpecHelper with FileUploadFixtures {
@@ -44,39 +43,6 @@ class FileUploadJourneyRepositoryISpec extends ComponentSpecHelper with FileUplo
   }
 
 
-  ".calling .getFormFieldsForFile()" should {
-
-    s"return $None when the uploadDetails does not exist" in {
-      await(repository.getFormFieldsForFile(testJourneyId, fileRef1)) shouldBe None
-    }
-
-    s"return $Some upload fields when the uploadDetails exists" in {
-      await(repository.upsertFileUpload(testJourneyId, callbackModel))
-      await(repository.getFormFieldsForFile(testJourneyId, fileRef1)) shouldBe callbackModel.uploadFields
-    }
-  }
-
-
-  "calling .getStatusOfFileUpload()" should {
-
-    s"return $None when the document is not in Mongo" in {
-      await(repository.getStatusOfFileUpload(testJourneyId, "")) shouldBe None
-    }
-
-    s"return $Some when the document is in Mongo" in {
-      await(repository.upsertFileUpload(testJourneyId, callbackModel))
-      await(repository.getStatusOfFileUpload(testJourneyId, fileRef1)) shouldBe
-        Some(UploadStatus(UploadStatusEnum.READY.toString))
-    }
-
-    s"return $Some when the document is in Mongo (failed upload)" in {
-      await(repository.upsertFileUpload(testJourneyId, callbackModelFailed))
-      await(repository.getStatusOfFileUpload(testJourneyId, fileRef1)) shouldBe
-        Some(UploadStatus(FailureReasonEnum.QUARANTINE.toString, Some(testVirusMessage)))
-    }
-  }
-
-
   "calling .getAllFiles()" should {
 
     s"return empty sequence when the document is not in Mongo" in {
@@ -87,57 +53,6 @@ class FileUploadJourneyRepositoryISpec extends ComponentSpecHelper with FileUplo
       await(repository.upsertFileUpload(testJourneyId, callbackModel))
       await(repository.upsertFileUpload(testJourneyId, callbackModel2))
       await(repository.getAllFiles(testJourneyId)) shouldBe Seq(callbackModel, callbackModel2)
-    }
-  }
-
-
-  "calling .getNumberOfReadyFiles()" should {
-
-    "return 0 when there is no uploads for the journey" in {
-      await(repository.getNumberOfReadyFiles(testJourneyId)) shouldBe 0
-    }
-
-    "return 0 when there is no uploads that are READY for the journey" in {
-      await(repository.upsertFileUpload(testJourneyId, waitingFile))
-      await(repository.getNumberOfReadyFiles(testJourneyId)) shouldBe 0
-    }
-
-    "return a count of all READY uploads when there are some" in {
-      await(repository.upsertFileUpload(testJourneyId, waitingFile))
-      await(repository.upsertFileUpload(testJourneyId, callbackModel2))
-      await(repository.upsertFileUpload(testJourneyId, callbackModel2.copy(reference = "ref3")))
-      await(repository.upsertFileUpload(testJourneyId, waitingFile.copy(reference = "ref4")))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 4
-      await(repository.getNumberOfReadyFiles(testJourneyId)) shouldBe 2
-    }
-  }
-
-
-  "calling .removeFileForJourney()" should {
-
-    "remove the file in the journey if it exists" in {
-      await(repository.upsertFileUpload(testJourneyId, callbackModel))
-      await(repository.upsertFileUpload(testJourneyId, callbackModel2))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 2
-      await(repository.removeFile(testJourneyId, fileRef1))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 1
-      await(repository.getAllFiles(testJourneyId)).headOption shouldBe Some(callbackModel2)
-    }
-
-    "do not remove the file in the journey if the file specified doesn't exist" in {
-      await(repository.upsertFileUpload(testJourneyId, callbackModel))
-      await(repository.upsertFileUpload(testJourneyId, callbackModel2))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 2
-      await(repository.removeFile(testJourneyId, "ref1234"))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 2
-    }
-
-    "do not remove the file in the journey if the journey specified doesn't exist" in {
-      await(repository.upsertFileUpload(testJourneyId, callbackModel))
-      await(repository.upsertFileUpload(testJourneyId, callbackModel2))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 2
-      await(repository.removeFile("1235", fileRef1))
-      await(repository.getTotalNumberOfFiles(testJourneyId)) shouldBe 2
     }
   }
 
