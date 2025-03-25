6f16f6dcb54fe222b066e2bec5d3c40cd3571426
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-03-25 09:37:12 +0000

    [MIPR-1701] Address the future bobby violations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,12 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "11.12.0",
-    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % "1.9.0",
-    "uk.gov.hmrc"        %% "crypto-json-play-30"        % "7.6.0"
+    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % "2.6.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"    %% "bootstrap-test-play-30"     % bootstrapVersion    % Test,
-    "org.jsoup"      %  "jsoup"                      % "1.18.1"            % Test
+    "org.jsoup"      %  "jsoup"                      % "1.19.1"            % Test
   )
 
   val it: Seq[Nothing] = Seq.empty
