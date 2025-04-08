44cb53e0901dbce709f34f940a37dbbe7848d77b
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2025-04-08 17:24:44 +0100

    MIPR-1692: play-frontend-hmrc-play-30 updated to 11.13.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "11.12.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "11.13.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % "2.6.0"
   )
 
