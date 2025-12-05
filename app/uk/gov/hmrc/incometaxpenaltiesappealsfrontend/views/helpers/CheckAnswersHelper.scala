a7f1099365046b228c2d822fc64786322af8c3bd
kSiadat <49532979+kSiadat@users.noreply.github.com>
2025-12-05 11:20:47 +0000

    MIPR-2285: re-ordered CYA SummaryList

--- app/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/views/helpers/CheckAnswersHelper.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/views/helpers/CheckAnswersHelper.scala
@@ -37,9 +37,9 @@ class CheckAnswersHelper @Inject()(lateAppealSummary: LateAppealSummary) {
       WhenDidEventEndSummary.row(showActionLinks),
       CrimeReportedSummary.row(showActionLinks),
       MissedDeadlineReasonSummary.row(showActionLinks),
-      ReviewMoreThan30DaysSummary.row(showActionLinks),
-      lateAppealSummary.row(showActionLinks),
       ExtraEvidenceSummary.row(showActionLinks),
-      UploadedDocumentsSummary.row(uploadedFiles, showActionLinks)
+      UploadedDocumentsSummary.row(uploadedFiles, showActionLinks),
+      ReviewMoreThan30DaysSummary.row(showActionLinks),
+      lateAppealSummary.row(showActionLinks)
     ).flatten
 }
