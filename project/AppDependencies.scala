dcb1b72512c2056f04153b2b8c18150e88e55ad4
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2026-01-08 16:04:00 +0000

    app dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.4.0"
+  private val bootstrapVersion = "10.5.0"
 
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.22.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.25.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.11.0"
   )
 
