2f7949e78771a1d60d7db29ee77ea0864b9e197d
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-02-09 11:59:23 +0000

    Amend content passive language and contractions

--- it/test/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/controllers/AppealStartControllerISpec.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesappealsfrontend/controllers/AppealStartControllerISpec.scala
@@ -61,10 +61,9 @@ class AppealStartControllerISpec extends ControllerISpecHelper {
               dateToString(lateSubmissionAppealData.endDate)
             )
             document.getH1Elements.text() shouldBe "Appeal a Self Assessment penalty"
-            document.getParagraphs.get(0).text() shouldBe "To appeal a late submission penalty for Self Assessment, you’ll need to ask HMRC to look at your case again."
-            document.getParagraphs.get(1).text() shouldBe "This service is for appealing penalties given for individual submissions."
+            document.getParagraphs.get(0).text() shouldBe "To appeal a late submission penalty for Self Assessment, you will need to ask HMRC to look at your case again."
             document.getH2Elements.get(0).text() shouldBe "Before you start"
-            document.getParagraphs.get(2).text() shouldBe "You’ll need:"
+            document.getParagraphs.get(1).text() shouldBe "You will need:"
             document.getBulletPoints.get(0).text() shouldBe "a reason why the quarterly update or tax return was not submitted by the due date (HMRC calls this ‘a reasonable excuse’)"
             document.getBulletPoints.get(1).text() shouldBe "the dates related to this reasonable excuse"
             document.getBulletPoints.get(2).text() shouldBe "details of why you did not appeal sooner"
@@ -72,24 +71,24 @@ class AppealStartControllerISpec extends ControllerISpecHelper {
             document.getH2Elements.get(1).text() shouldBe "Income sources that have ceased"
 
             if(isAgent){
-              document.getParagraphs.get(4).text() shouldBe "If your client has received a penalty for an update period that started after the income source ceased, they may be able to get that point removed."
+              document.getParagraphs.get(3).text() shouldBe "If your client has received a penalty for an update period that started after the income source had stopped permanently (ceased), they may be able to get that point removed."
 
             } else {
-              document.getParagraphs.get(4).text() shouldBe "If you have received a penalty for an update period that started after the income source ceased, you may be able to get that point removed."
+              document.getParagraphs.get(3).text() shouldBe "If you have received a penalty for an update period that started after the income source had stopped permanently (ceased), you may be able to get that point removed."
             }
 
             if(isAgent){
-              document.getParagraphs.get(5).text() shouldBe "To do this, they will need to confirm the dates that a particular income source was ceased to HMRC."
+              document.getParagraphs.get(4).text() shouldBe "To do this, they will need to confirm the dates that a particular income source was ceased to HMRC."
 
             } else {
-              document.getParagraphs.get(5).text() shouldBe "To do this, you will need to confirm the date that a particular income source was ceased to HMRC."
+              document.getParagraphs.get(4).text() shouldBe "To do this, you will need to confirm the date that a particular income source was ceased to HMRC."
             }
 
             document.getLink("cessationLink").text() shouldBe "Add, manage or cease a business or income source"
 
             document.getH2Elements.get(2).text() shouldBe "Sending evidence with an appeal"
-            document.getParagraphs.get(7).text() shouldBe "In some cases, you’ll be asked if you want to upload evidence to support your appeal. You should gather this evidence before you continue, as you will not be able to save this appeal and complete it later."
-            document.getParagraphs.get(8).text() shouldBe "If you are not asked for extra evidence, this is because we don’t need any to make a decision in your particular case."
+            document.getParagraphs.get(6).text() shouldBe "In some cases, you will be asked if you want to upload evidence to support your appeal. You should gather this evidence before you continue, as you will not be able to save this appeal and complete it later."
+            document.getParagraphs.get(7).text() shouldBe "If you are not asked for extra evidence, this is because we do not need any to make a decision in your particular case."
             document.getWarningText.get(0).text() shouldBe "Warning If we decide we need extra evidence after reviewing your appeal, we will contact you."
 
             document.getSubmitButton.text() shouldBe "Start an appeal"
@@ -115,16 +114,15 @@ class AppealStartControllerISpec extends ControllerISpecHelper {
               dateToString(latePaymentAppealData.endDate)
             )
             document.getH1Elements.text() shouldBe "Appeal a Self Assessment penalty"
-            document.getParagraphs.get(0).text() shouldBe "To appeal a late payment penalty for Self Assessment, you’ll need to ask HMRC to look at your case again."
-            document.getParagraphs.get(1).text() shouldBe "This service is for appealing penalties given for individual submissions."
+            document.getParagraphs.get(0).text() shouldBe "To appeal a late payment penalty for Self Assessment, you will need to ask HMRC to look at your case again."
             document.getH2Elements.get(0).text() shouldBe "Before you start"
-            document.getParagraphs.get(2).text() shouldBe "You’ll need:"
+            document.getParagraphs.get(1).text() shouldBe "You will need:"
             document.getBulletPoints.get(0).text() shouldBe "a reason why the tax payment was not made by the due date (HMRC calls this ‘a reasonable excuse’)"
             document.getBulletPoints.get(1).text() shouldBe "the dates related to this reasonable excuse"
             document.getBulletPoints.get(2).text() shouldBe "details of why you did not appeal sooner"
             document.getLink("guidanceLink").text() shouldBe "Read the guidance about reasonable excuses (opens in new tab)"
-            document.getParagraphs.get(4).text() shouldBe "In some cases, you’ll be asked if you want to upload evidence to support your appeal. You should gather this evidence before you continue, as you will not be able to save this appeal and complete it later."
-            document.getParagraphs.get(5).text() shouldBe "If you are not asked for extra evidence, this is because we don’t need any to make a decision in your particular case."
+            document.getParagraphs.get(3).text() shouldBe "In some cases, you will be asked if you want to upload evidence to support your appeal. You should gather this evidence before you continue, as you will not be able to save this appeal and complete it later."
+            document.getParagraphs.get(4).text() shouldBe "If you are not asked for extra evidence, this is because we do not need any to make a decision in your particular case."
             document.getWarningText.get(0).text() shouldBe "Warning If we decide we need extra evidence after reviewing your appeal, we will contact you."
             document.getSubmitButton.text() shouldBe "Continue"
             document.getSubmitButton.attr("href") shouldBe routes.ReasonableExcuseController.onPageLoad(isAgent, NormalMode).url
@@ -146,16 +144,15 @@ class AppealStartControllerISpec extends ControllerISpecHelper {
               dateToString(latePaymentAppealData.endDate)
             )
             document.getH1Elements.text() shouldBe "Appeal a Self Assessment penalty"
-            document.getParagraphs.get(0).text() shouldBe "To appeal a late payment penalty for Self Assessment, you’ll need to ask HMRC to look at your case again."
-            document.getParagraphs.get(1).text() shouldBe "This service is for appealing penalties given for individual submissions."
+            document.getParagraphs.get(0).text() shouldBe "To appeal a late payment penalty for Self Assessment, you will need to ask HMRC to look at your case again."
             document.getH2Elements.get(0).text() shouldBe "Before you start"
-            document.getParagraphs.get(2).text() shouldBe "You’ll need:"
+            document.getParagraphs.get(1).text() shouldBe "You will need:"
             document.getBulletPoints.get(0).text() shouldBe "a reason why the tax payment was not made by the due date (HMRC calls this ‘a reasonable excuse’)"
             document.getBulletPoints.get(1).text() shouldBe "the dates related to this reasonable excuse"
             document.getBulletPoints.get(2).text() shouldBe "details of why you did not appeal sooner"
             document.getLink("guidanceLink").text() shouldBe "Read the guidance about reasonable excuses (opens in new tab)"
-            document.getParagraphs.get(4).text() shouldBe "In some cases, you’ll be asked if you want to upload evidence to support your appeal. You should gather this evidence before you continue, as you will not be able to save this appeal and complete it later."
-            document.getParagraphs.get(5).text() shouldBe "If you are not asked for extra evidence, this is because we don’t need any to make a decision in your particular case."
+            document.getParagraphs.get(3).text() shouldBe "In some cases, you will be asked if you want to upload evidence to support your appeal. You should gather this evidence before you continue, as you will not be able to save this appeal and complete it later."
+            document.getParagraphs.get(4).text() shouldBe "If you are not asked for extra evidence, this is because we do not need any to make a decision in your particular case."
             document.getWarningText.get(0).text() shouldBe "Warning If we decide we need extra evidence after reviewing your appeal, we will contact you."
 
             document.getSubmitButton.text() shouldBe "Continue"
