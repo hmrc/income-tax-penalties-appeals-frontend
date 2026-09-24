3ce5b73607589e5dfc152902a313d2db793bf91a
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-09-24 11:06:13 +0100

    MIPR-dep: dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
 
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.9.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.12.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.14.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.14.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.jsoup"      % "jsoup"                  % "1.22.2"         % Test,
+    "org.jsoup"      % "jsoup"                  % "1.23.2"         % Test,
     "org.scalamock" %% "scalamock"              % "7.5.5"          % Test,
   )
 
